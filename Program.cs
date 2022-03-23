681f13069874989f556374d7fc3565c4fc0bad04
Mahmoud Da'san <mahmoud.dasan@outlook.com>
2022-03-23 10:17:27 +0300

    m1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
