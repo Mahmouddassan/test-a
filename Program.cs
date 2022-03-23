e835ac63128ac757f76a835e1a9c722c20298d86
Mahmoud Da'san <mahmoud.dasan@outlook.com>
2022-03-23 11:06:35 +0300

    last update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace project_a
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("m2");
+            Console.WriteLine("m3");
+
+            Console.WriteLine("b1- m1");
+            Console.WriteLine("b1- m2");
+
+            Console.WriteLine("m4");
+
+            Console.WriteLine("b2");
+            Console.WriteLine("b2-m2");
+            Console.WriteLine("hi");
         }
     }
 }
